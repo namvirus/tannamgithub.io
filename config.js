a690f5f4d33cb105a2f66ac0b55a9ed3bfce4ff8
namvirus <95855416+namvirus@users.noreply.github.com>
2021-12-09 17:55:53 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Đào Lê Thanh Hiền <3 Trương Công Tấn Nam',
+    desc: 'Trương Thanh Vy ',
+    btnYes: 'chọn yêu anh mãi mãi <33',
+    btnNo: 'Không thèm yêu a :3',
+    question:'Em là thế giới của a <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu ông xã nhiều lắm  <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay a chở bà xã về nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100013455458557' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
